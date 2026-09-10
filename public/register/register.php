503f5772b8a88697892538c27f916ffe11fb40fa
Arthur de Oliveira Rabello <20251CTI-NH0060@nh.ifsul.edu.br>
2026-09-10 13:23:33 -0300

    fix: style to brutalism

---
+++ public/register/register.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="register.css">
+    <title>Página de Registro</title>
+</head>
+<body>
+
+    <div class="login-container">
+        <img src="../midia/img/ifsullogo/ifsul-logo.png" alt="logo if">
+        <h1>Registro</h1>
+        <form method="POST" action="../../src/data/pcs_Register.php">
+            <label for="username" class="labels">Email:</label>
+            <input id="username" type="text" name="username" placeholder="Digite seu email" required>
+
+            <label for="password" class="labels">Senha:</label>
+            <input id="password" type="password" name="password" placeholder="Crie sua senha" required>
+
+            <button id="login-btn" type="submit">Registar-se</button>
+        </form>
+
+        <footer>
+            
+        </footer>
+    </div>
+</body>
+</html>
