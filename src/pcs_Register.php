18cba90ac1e4b1e02dc6f19a2b74d31bfdb34bcf
Arthur de Oliveira Rabello <20251CTI-NH0060@nh.ifsul.edu.br>
2026-09-10 13:59:14 -0300

    pcs_Register

--- src/pcs_Register.php
+++ src/pcs_Register.php
@@ -1,6 +1,45 @@
 <?php
 
+require_once __DIR__ . '/../data/Connect.php';
+
+function checkData($data, array $requiredFields){
+    foreach($requiredFields as $field){
+        if(empty($data[$field])){
+            return false;
+        }
+    }
+    return true;
+}
+
+function disconnect(){
+    $pdo = null;
+}
+
+function connect(){
+    $pdo = Connect::getInstance();
+    return $pdo;
+}
+
+function insertData($pdo, array $requiredFields){
+    $userlogin = $_POST['username'];
+    $password = $_POST['password'];
+    
+    $password = password_hash($password);
+
+    $stmt = $pdo->prepare("INSERT INTO user (user_email, user_password) VALUES (:user_email, :user_password)");
+    $stmt->bindParam(':user_email', $userlogin);
+    $stmt->bindParam(':user_password', $password);
+    $stmt->execute();
+}
+
 //-------------------------
 //         CÓDIGO
 //-------------------------
 
+$requiredFields = ['username', 'password'];
+
+if(checkData($_POST, $requiredFields)){
+    $pdo = connect();
+    insertData($pdo, $requiredFields);
+    disconnect();
+}
